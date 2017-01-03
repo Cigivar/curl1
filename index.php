fbf98449fb5a92e2d95a8dacb0125048a7d161a3
iMacros <newnagval@hotmail.com>
2017-01-04 02:38:53 +0300

    6
    Выкачиваем страницу с первым курсом

--- index.php
+++ index.php
@@ -3,14 +3,21 @@
     include_once('lib/curl_query.php');
     include_once('lib/simple_html_dom.php');
 
-$html = curl_get('http://ntschool.ru/kursyi');
+$html = curl_get('http://www.avalon.ru/Courses/Design/Courses/?gclid=CKa5h6OMp9ECFciCsgodJHACoQ');
 $dom = str_get_html($html);
 
-$courses = $dom -> find('.courses-list--item-body');
+$courses = $dom -> find('.emphasis');
 
 
 
-foreach($courses as $course){
+foreach ($courses as $course){
     $a = $course -> find('a',0);
-    echo $a -> href."<br />";
+   echo $a -> href.'<br>'; 
+    
+    $one = curl_get('http://www.avalon.ru/Courses/Design/Courses/?gclid=CKa5h6OMp9ECFciCsgodJHACoQ'.$a -> href);
+    
+    file_put_contents('1.html',$one);
+    break;
+    
+    
 }
